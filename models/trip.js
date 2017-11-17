896f3907b817bb351c3c63ba010ac0cb9e409137
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-11-17 15:55:32 -0800

    Built out Flight model.

--- models/trip.js
+++ models/trip.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var tripSchema = new Schema ({
   departureCity: {type: String, required: true},
   arrivalCity: {type:String, required: true},
-  travelers: {type: String, required: true},
+  travelers: {type: String, required: true, default: "1"},
   flights: {type: Schema.Types.ObjectId, ref: 'Flight'},
   hotels: {type: Schema.Types.ObjectId, ref: 'Hotel'}
   },
