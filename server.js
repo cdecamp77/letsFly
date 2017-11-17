60174ffc02284511cb44a6ff66b8cd63d456707f
Antonio Navarro <navarroaa90@gmail.com>
2017-11-17 12:07:55 -0800

    sessions loaded

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 // load the env vars
 require('dotenv').config();
 
@@ -28,6 +28,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'letsfly',
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
