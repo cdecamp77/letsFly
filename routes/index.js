6b473823709b32bc9b35dabee0c1092a4bd0e076
Antonio Navarro <navarroaa90@gmail.com>
2017-11-17 12:34:52 -0800

    updated index routes oAuth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
+var passport = require('passport');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/auth/google', passport.authenticate(
+  'google', 
+  { scope: ['profile', 'email']}
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/', function(req, res) {
+  res.render('index', { user: req.user });
+});
+
 
 module.exports = router;
