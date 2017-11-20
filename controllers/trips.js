b24a340d44c03ccc4a205da14b722f6f937a0294
Antonio Navarro <navarroaa90@gmail.com>
2017-11-20 15:00:34 -0800

    added addEvent for google calendar

--- controllers/trips.js
+++ controllers/trips.js
@@ -2,6 +2,7 @@ var Trip = require('../models/trip');
 var Hotel = require('../models/hotel');
 var Flight = require('../models/flight');
 var request = require('request');
+var calendar = require('../utilities/google-calendar');
 
 function root (req, res) {
     res.render('index', { user: req.user });
@@ -17,6 +18,17 @@ function hotelSearch (req, res) {
 
 function index (req, res) {
     Trip.find({}).populate('Flight').populate('Hotel').exec((err, trips) => {
+        // calendar.addEvent(req.user.googleToken, 'This is my new event', new Date('11/21/2017 17:00' ).toISOString(), new Date('11/21/2017 19:00').toISOString())
+        // .then(function(events) {
+        //     console.log(events);
+        // });
+        // calendar.listEvents(req.user.googleToken)
+        // .then(function(response) {
+        //     console.log(response);
+        // })
+        // .catch(function(err) {
+        //     console.log(err);
+        // });
         res.render('./users/dash', { trips, user: req.user });
     });
 }
@@ -36,5 +48,6 @@ module.exports = {
     flightSearch,
     hotelSearch,
     index,
-    insp
+    insp,
+    getAmadeusData
 }
