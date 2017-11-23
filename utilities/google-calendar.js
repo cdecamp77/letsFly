84ff366d61e7de0f9222061df7e8b0fdb2e388b8
Antonio Navarro <navarroaa90@gmail.com>
2017-11-22 17:20:03 -0800

    added google calendar to booking, not yet working

--- utilities/google-calendar.js
+++ utilities/google-calendar.js
@@ -1,43 +1,43 @@
-// var request = require('request-promise-native');
+var request = require('request-promise-native');
 
-// function listEvents(token) {
-//     var options = {
-//         headers: {
-//             Authorization: `Bearer ${token}`
-//         },
-//         uri: 'https://www.googleapis.com/calendar/v3/calendars/primary/events'
-//      };
-//     return request(options);
-// }
-
-
-// function addEvent(token, summary, start, end){
-//     var options = {
-//         headers: {
-//             Authorization: `Bearer ${token}`,
-//             'Content-type': 'application/json'
-//         },
-//         method: 'POST',
-//         json: true,
-//         body: { 
-//             'summary': summary,
-//             'start': {
-//                 'dateTime': start,
-//                 },
-//                 'end': {
-//                 'dateTime': end
-//                 },
-//         },
-//         uri: 'https://www.googleapis.com/calendar/v3/calendars/primary/events'
-//     };
-//     return request(options);
-// }
+function listEvents(token) {
+    var options = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        uri: 'https://www.googleapis.com/calendar/v3/calendars/primary/events'
+     };
+    return request(options);
+}
 
 
-// module.exports = {
-//     listEvents , 
-//     addEvent,
-// };
+function addEvent(token, summary, start, end){
+    var options = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        json: true,
+        body: { 
+            'summary': summary,
+            'start': {
+                'dateTime': start,
+                },
+                'end': {
+                'dateTime': end
+                },
+        },
+        uri: 'https://www.googleapis.com/calendar/v3/calendars/primary/events'
+    };
+    return request(options);
+}
+
+
+module.exports = {
+    listEvents , 
+    addEvent,
+};
 
 
 // var options = {
