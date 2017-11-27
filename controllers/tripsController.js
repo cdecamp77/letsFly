5ee45be92d0f3551b0ad83fb19ba8b71d8dc784d
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-11-27 11:54:35 -0800

    Updated date storage in Trip model, and date prefills on edit trip functionality.

--- controllers/tripsController.js
+++ controllers/tripsController.js
@@ -14,7 +14,7 @@ function tripSearch (req, res) {
 function createTripAndGetFlightData(req, res) {
     var body = req.body;
     var retDate;
-    var newTrip = new Trip({departureCity: body.departureCity, arrivalCity: body.arrivalCity, travelers: body.travelers, departureDate: Date.parse(body.departureDate), returnDate: Date.parse(body.returnDate)});
+    var newTrip = new Trip({departureCity: body.departureCity, arrivalCity: body.arrivalCity, travelers: body.travelers, departureDate: new Date(body.departureDate.split('-').join(',')), returnDate: new Date(body.returnDate.split('-').join(','))});
     newTrip.save(err => {
         if (err) return res.render('./flights/search', {user: req.user});
         req.user.trips.push(newTrip);
@@ -34,8 +34,8 @@ function bookTripFlights(req, res) {
     Trip.findById(req.params.id, (err, trip) => {
         for (flight in body) {
             var flightObject = body[flight];
-            flightObject.departureTime = Date.parse(flightObject.departureTime);
-            flightObject.arrivalTime = Date.parse(flightObject.arrivalTime);
+            flightObject.departureTime = new Date(flightObject.departureTime);
+            flightObject.arrivalTime = new Date(flightObject.arrivalTime);
             trip.flights.push(flightObject);
         }
         trip.save( err => {
@@ -65,7 +65,7 @@ function editTrip(req, res) {
 function editTripAndGetNewFlights(req, res) {
     var body = req.body;
     var retDate;
-    Trip.findByIdAndUpdate(req.params.id, {departureCity: body.departureCity, arrivalCity: body.arrivalCity, travelers: body.travelers, departureDate: Date.parse(body.departureDate), returnDate: Date.parse(body.returnDate)}, (err, trip) => {
+    Trip.findByIdAndUpdate(req.params.id, {departureCity: body.departureCity, arrivalCity: body.arrivalCity, travelers: body.travelers, departureDate: new Date(body.departureDate.split('-').join(',')), returnDate: new Date(body.returnDate.split('-').join(','))}, (err, trip) => {
         if (err) return res.render(`/trips/${req.params.id}/edit`, {user: req.user});
         !body.returnDate ? retDate = '' : retDate = `&return_date=${body.returnDate}`;
         request(`https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=${process.env.AMADEUS_TOKEN}&origin=${body.departureCity.slice(-4, -1)}&destination=${body.arrivalCity.slice(-4, -1)}&departure_date=${body.departureDate}&adults=${body.travelers}&number_of_results=20${retDate}`, (err, response, flights) => {
@@ -80,8 +80,8 @@ function editTripBookedFlights(req, res) {
     Trip.findByIdAndUpdate(req.params.id, { $set: { flights: [] }}, (err, trip) => {
         for (flight in body) {
             var flightObject = body[flight];
-            flightObject.departureTime = Date.parse(flightObject.departureTime);
-            flightObject.arrivalTime = Date.parse(flightObject.arrivalTime);
+            flightObject.departureTime = new Date(flightObject.departureTime);
+            flightObject.arrivalTime = new Date(flightObject.arrivalTime);
             trip.flights.push(flightObject);
         }
         trip.save( err => {
